ea0430634d2a55cd6f4147ebadc238261e0b36ec
Seda Demirel <sedeydem@gmail.com>
2025-04-24 16:44:45 +0300

    menu icon dısarı tasındı

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -1,30 +1,34 @@
 import { useState } from 'react';
-import React from 'react'
-import '../Navbar/Navbar.css'
+import React from 'react';
+import '../Navbar/Navbar.css';
 import { Link } from 'react-scroll';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
-
-
 function Navbar() {
-
   const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => setMenuOpen(!menuOpen);
   const closeMenu = () => setMenuOpen(false);
 
-    const menuItems = [
-        { id: 'home', label: 'Home' },
-        { id: 'services', label: 'Services' },
-        { id: 'about', label: 'About Me' },
-        { id: 'experience', label: 'Experience' },
-        { id:'project', label:'Project'},
-        { id: 'skills' , label:'Skills'}
-      ];
+  const menuItems = [
+    { id: 'home', label: 'Home' },
+    { id: 'services', label: 'Services' },
+    { id: 'about', label: 'About Me' },
+    { id: 'experience', label: 'Experience' },
+    { id: 'project', label: 'Project' },
+    { id: 'skills', label: 'Skills' }
+  ];
+
   return (
     <nav className='navbar'>
       <div className='logo'>My Portfolio</div>
-      <div className='menu-icon' onClick={toggleMenu}>{menuOpen ? <FaTimes/> : <FaBars/>}</div>
+
+      {/* Menü butonu */}
+      <div className='menu-icon' onClick={toggleMenu}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
+      {/* Menü linkleri */}
       <ul className={`nav-links ${menuOpen ? 'active' : ''}`}>
         {menuItems.map((item) => (
           <li key={item.id}>
@@ -42,9 +46,18 @@ function Navbar() {
           </li>
         ))}
       </ul>
-      <a  href="/SedaDemirelCV.pdf" download className='cv-button' onClick={closeMenu}>Download CV</a>
+
+      {/* CV Butonu (sadece masaüstünde görünür) */}
+      <a
+        href="/SedaDemirelCV.pdf"
+        download
+        className='cv-button'
+        onClick={closeMenu}
+      >
+        Download CV
+      </a>
     </nav>
-)
+  );
 }
 
-export default Navbar
+export default Navbar;
